ee78c0d2a2944fd0d93fd29e3e6814cbba222c1b
85start <christopherfalcon491@gmail.com>
2024-02-19 19:57:11 -0800

    comments and post button

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,14 @@
-import Post from "./Post";
-
-
 import React, { useState } from 'react';
 
 const HomePage = () => {
   const [beans, setBeans] = useState([]);
+  const [comment, setComment] = useState('');
+  
+  const [showCommentContainer, setShowCommentContainer] = useState(false);
+  const [showPostContainer, setShowPostContainer] = useState(false);
+  
+  const [comments, setComments] = useState([]);
+  const [posts, setPosts] = useState([]); // State for posts
 
   const handleAddBean = (e) => {
     if (e.key === 'Enter' && !e.shiftKey) {
@@ -28,13 +32,29 @@ const HomePage = () => {
     }
   };
 
+  const commentHandler = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmitComment = () => {
+    console.log('Submitted Comment:', comment);
+    setComments([...comments, comment]);
+    setComment('');
+  };
+
+  const handleSubmitPost = () => {
+    console.log('Submitted Post:', beans); // You can adjust this to handle post submission
+    setPosts([...posts, beans]);
+    setBeans([]);
+  };
+
   return (
     <div className="home">
       <div className="square-wrapper">
         <div className="square">
           {beans.map((bean, index) => (
             bean.type === 'text' ? <div key={index}>{bean.content}</div> :
-            <img key={index} src={bean.content} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '800' }} />
+              <img key={index} src={bean.content} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '800' }} />
           ))}
         </div>
         <div className="addToBean-container">
@@ -50,6 +70,34 @@ const HomePage = () => {
           />
         </div>
       </div>
+
+      <button onClick={() => setShowCommentContainer(!showCommentContainer)}>Comment</button>
+      {showCommentContainer && (
+        <div>
+          {comments.map((comment, index) => (
+            <div key={index} className="CommentContainer">{comment}</div>
+          ))}
+          <div className="Comment-flex">
+            <textarea value={comment} onChange={commentHandler} className="input-box" />
+            <button onClick={handleSubmitComment}>Submit Comment</button>
+          </div>
+        </div>
+      )}
+
+      <button onClick={() => setShowPostContainer(!showPostContainer)}>Post</button>
+      {showPostContainer && (
+        <div>
+          {posts.map((post, index) => (
+            <div key={index} className="PostContainer">
+              {post.map((bean, index) => (
+                bean.type === 'text' ? <div key={index}>{bean.content}</div> :
+                  <img key={index} src={bean.content} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '800' }} />
+              ))}
+            </div>
+          ))}
+          <button onClick={handleSubmitPost}>Submit Post</button>
+        </div>
+      )}
     </div>
   );
 };
