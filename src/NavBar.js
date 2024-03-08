001a21fb6952a843e2d397491124d05def265add
Drew Wan <drewkeithwan@gmail.com>
2024-03-08 14:30:27 -0800

    Added drop down menu and moved profile picture

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,27 +1,38 @@
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from './Pictures/Fridayz-Smaller.png'; 
-import './index.css'; // Adjust the path as needed
-
+import './index.css';
 
 const Navbar = () => {
-  const history = useNavigate();
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
-  const handleClick = () => {
-    history.navigate("/");
+  // Function to toggle dropdown visibility
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
   };
 
   return (
     <nav className="navbar">
       <Link to="/">
-        <img src= {logo} alt="Fridayz" className="logo-small" />
+        <img src={logo} alt="Fridayz" className="logo-small" />
       </Link>
       <div className="links">
-        <Link to="/profile">Profile</Link>
-        <Link to="/friends">Friends</Link>
-        <Link to="/login">Login</Link>
+        {/* Dropdown toggle button */}
+        <div onClick={toggleDropdown} className="profile-nav-container">
+          <img src="https://via.placeholder.com/150" alt="Profile" className="profile-nav"/>
+          {isDropdownVisible && (
+            <div className="dropdown-menu">
+              <Link to="/friends">Friends</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/profile">Profile</Link>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
+
