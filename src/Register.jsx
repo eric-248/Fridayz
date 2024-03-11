d5e3b892dcb2847f8ad8e6f76f7129e6d8d4d78d
Eric Du <ericdu117@gmail.com>
2024-03-11 10:40:12 -0700

    Fixed typing fields for login and registration page

--- src/Register.jsx
+++ src/Register.jsx
@@ -22,23 +22,43 @@ export const Register = () => {
 
         <div className="registration-page">
             
-            <div className="auth-from-container">
+            <div className="auth-form-container">
 
                 <h2>Register</h2>
                 
                 <form className="registration-form" onSubmit={handleSubmit}>
 
                 {/* Name*/}
-                <label for="name">Name</label>
-                <input value={name} name="name" id="name" placeholder="Full Name" />
+                <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    name="name"
+                    id="name"
+                    placeholder="Full Name"
+                />
 
                 {/*Email*/}
-                <label for="email">Email</label>
-                <input value = {email} type="email" placeholder="abc@gmail.com" id="email" name="email"/>
+                <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    placeholder="abc@gmail.com"
+                    id="email"
+                    name="email"
+                />
 
                 {/*Password*/}
-                <label for="password">Password</label>
-                <input value = {pass} type="password" placeholder="*********" id="password" name="password"/>
+                <input
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                    type="password"
+                    placeholder="********"
+                    id="password"
+                    name="password"
+                />
+               
+                {/*Register Button */}
+                <button>Register</button>
 
                 {/*Login if already have account*/}
                 <Link to="/login" style={{color: 'white'}}>Already have an account? Login.</Link>
