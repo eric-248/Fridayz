008d7747caa2c067c909ad32e2073e461e2a3a4e
laurenrm3 <115776428+laurenrm3@users.noreply.github.com>
2024-03-12 18:04:24 -0700

    mongodb backend connected

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Beans from './Beans';
-import Comments from './Comments';
-import Posts from './Posts';
+import React, { useState } from "react";
+import Beans from "./Beans";
+import Comments from "./Comments";
+import Posts from "./Posts";
 
 const HomePage = () => {
   const [beans, setBeans] = useState([]);
@@ -11,19 +11,41 @@ const HomePage = () => {
     setBeans((prevBeans) => [...prevBeans, bean]);
   };
 
-  const handlePostBeans = () => {
+  const handlePostBeans = async () => {
     setShowPosts(true); // Update state to show posts
+
+    e.preventDefault();
+    let result = await fetch("http://localhost:5000/register", {
+      method: "post",
+      body: JSON.stringify({ name, email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    if (result) {
+      alert("Data saved succesfully");
+      setEmail("");
+      setName("");
+    }
   };
 
   return (
     <div>
-    <Beans addBean={addBean} />
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      {showPosts && <div style={{ marginRight: '50px', flex: 1 }}><Posts beans={beans} /></div>}
-      <div style={{ flex: 1 }}><Comments /></div>
+      <Beans addBean={addBean} />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        {showPosts && (
+          <div style={{ marginRight: "50px", flex: 1 }}>
+            <Posts beans={beans} />
+          </div>
+        )}
+        <div style={{ flex: 1 }}>
+          <Comments />
+        </div>
+      </div>
+      <button onClick={handlePostBeans}>It's Friday!</button>
     </div>
-    <button onClick={handlePostBeans}>It's Friday!</button>
-  </div>
   );
 };
 
